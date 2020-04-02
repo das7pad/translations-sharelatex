ff2c47f26ca9c13a021d66fa9bf747a7e2b1720e
Jakob Ackermann <das7pad@outlook.com>
2020-04-03 01:15:54 +0200

    [misc] cleanup unit tests and use correct url stubs, add https:// scheme
    see docs for
    
    - req.originalUrl: https://expressjs.com/en/api.html#req.originalUrl
    - req.url: https://nodejs.org/api/http.html#http_message_url
    
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- test/unit/js/translations-tests.js
+++ test/unit/js/translations-tests.js
@@ -6,34 +6,32 @@ const { expect } = require('chai')
 
 describe('translations', function() {
   beforeEach(function() {
-    this.settings = {}
     this.translations = SandboxedModule.require(modulePath, {
-      requires: {
-        'settings-sharelatex': this.settings,
-        'logger-sharelatex': { log() {} }
+      globals: {
+        process
       }
     })
 
     const opts = {
       subdomainLang: {
-        www: { lngCode: 'en', url: 'www.sharelatex.com' },
-        fr: { lngCode: 'fr', url: 'fr.sharelatex.com' },
-        da: { lngCode: 'da', url: 'da.sharelatex.com' }
+        www: { lngCode: 'en', url: 'https://www.sharelatex.com' },
+        fr: { lngCode: 'fr', url: 'https://fr.sharelatex.com' },
+        da: { lngCode: 'da', url: 'https://da.sharelatex.com' }
       }
     }
     this.translations = this.translations.setup(opts)
     this.req = {
-      originalUrl: "doesn'tmatter.sharelatex.com/login",
       headers: {
         'accept-language': ''
-      }
+      },
+      originalUrl: '/login',
+      url: '/login'
     }
     this.res = {}
   })
 
   describe('setLangBasedOnDomainMiddlewear', function() {
     it('should set the lang to french if the domain is fr', function(done) {
-      this.req.url = 'fr.sharelatex.com/login'
       this.req.headers.host = 'fr.sharelatex.com'
       this.translations.expressMiddlewear(this.req, this.req, () => {
         this.translations.setLangBasedOnDomainMiddlewear(
@@ -50,7 +48,6 @@ describe('translations', function() {
     describe('showUserOtherLng', function() {
       it('should set it to true if the languge based on headers is different to lng', function(done) {
         this.req.headers['accept-language'] = 'da, en-gb;q=0.8, en;q=0.7'
-        this.req.url = 'fr.sharelatex.com/login'
         this.req.headers.host = 'fr.sharelatex.com'
         this.translations.expressMiddlewear(this.req, this.req, () => {
           this.translations.setLangBasedOnDomainMiddlewear(
@@ -66,7 +63,6 @@ describe('translations', function() {
 
       it('should not set prop', function(done) {
         this.req.headers['accept-language'] = 'da, en-gb;q=0.8, en;q=0.7'
-        this.req.url = 'da.sharelatex.com/login'
         this.req.headers.host = 'da.sharelatex.com'
         this.translations.expressMiddlewear(this.req, this.req, () => {
           this.translations.setLangBasedOnDomainMiddlewear(
